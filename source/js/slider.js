3d28471f483c7564b1428d67a6d3d5ab6a2582f3
Daiver500 <60549770+Daiver500@users.noreply.github.com>
2021-02-26 21:10:25 +0300

    JS slider

--- source/js/slider.js
+++ source/js/slider.js
@@ -1,19 +1,27 @@
-const sliderButtonLeft = document.querySelector(".slider__button--left");
-const sliderButtonRight = document.querySelector(".slider__button--right");
-const sliderImage = document.querySelectorAll(".slider__picture")
+const slides = document.querySelectorAll(".slide");
+const slide = document.querySelector(".slide");
+const buttonRight = document.querySelector(".slider__button--right");
+const buttonLeft = document.querySelector(".slider__button--left");
+const slidesField = document.querySelector(".slider__wrapperinner")
+width = window.getComputedStyle(slide).width;
 
-const imageMoveRight = () => {
-  sliderImage.forEach((item) => {
-    item.style.transform = "translateX(-50%)"
-  })
-}
+let offset = 0;
 
-const imageMoveLeft = () => {
-  sliderImage.forEach((item) => {
-    item.style.transform = "translateX(50%)"
-  })
-}
+buttonRight.addEventListener("click", () => {
+  if (offset === +width.slice(0, width.length - 2) * (slides.length - 2)) {
+    offset = 0;
+  } else {
+    offset += +width.slice(0, width.length - 2);
+  }
+  slidesField.style.transform = `translateX(-${offset}px )`
+})
 
-sliderButtonRight.addEventListener("click", imageMoveRight);
-sliderButtonLeft.addEventListener("click", imageMoveLeft);
+buttonLeft.addEventListener("click", () => {
+  if (offset === 0) {
+    offset = +width.slice(0, width.length - 2) * (slides.length - 2)
+  } else {
+    offset -= +width.slice(0, width.length - 2);
+  }
+  slidesField.style.transform = `translateX(+${offset}px )`
+})
 
